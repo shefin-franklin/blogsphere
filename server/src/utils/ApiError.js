13e2b1ead5680404006fb1dfb3a911fd7b33c217
Shefin Franklin <shefinfranklin45@gmail.com>
2026-06-29 21:41:08 +0530

    Create ApiError.js

---
+++ server/src/utils/ApiError.js
@@ -0,0 +1,9 @@
+export class ApiError extends Error {
+  constructor(statusCode, message, details = null) {
+    super(message);
+    this.statusCode = statusCode;
+    this.isOperational = true;
+    this.details = details;
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
